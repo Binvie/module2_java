553932e55baafad57bc802413c0a9a134911d387
HoanThien <thien7.night1@gmail.com>
2023-06-06 17:22:10 +0700

    sd

---
+++ src/ss3/exercise/AddElement.java
@@ -0,0 +1,9 @@
+package ss3.exercise;
+
+public class AddElement {
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 6, 9, 11, 2};
+        int [] arrNew;
+
+    }
+}
--- src/ss3/exercise/DeleteElement.java
+++ src/ss3/exercise/DeleteElement.java
@@ -1,13 +1,24 @@
 package ss3.exercise;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class DeleteElement {
     public static void main(String[] args) {
-        int[] num = {10, 5, 6, 9, 11, 2};
+        int[] arrNum = {10, 5, 6, 9, 11, 2};
+        int[] arrNew;
+        arrNew = new int[arrNum.length];
         System.out.println("Input number that you wanna delete :");
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
-        
+        int index = 0;
+
+        for (int i = 0; i < arrNum.length; i++) {
+            if (number != arrNum[i]) {
+                arrNew[index] = arrNum[i];
+                index++;
+            }
+        }
+        System.out.println(Arrays.toString(arrNew));
     }
 }
