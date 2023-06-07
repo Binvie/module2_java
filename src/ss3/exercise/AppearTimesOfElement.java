1c1c91d66ad2685afd71f47664de420cea16fd70
HoanThien <thien7.night1@gmail.com>
2023-06-07 17:13:39 +0700

    baitap

---
+++ src/ss3/exercise/AppearTimesOfElement.java
@@ -0,0 +1,19 @@
+package ss3.exercise;
+
+import java.util.Scanner;
+
+public class AppearTimesOfElement {
+    public static void main(String[] args) {
+        String arr = "Hello em";
+        System.out.println("Enter element you want to count :");
+        Scanner sc = new Scanner(System.in);
+        char word = sc.nextLine().charAt(0);
+        int count = 0;
+        for (int i = 0; i < arr.length(); i++) {
+            if (word == arr.charAt(i)){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
---
+++ src/ss3/exercise/Program.java
@@ -0,0 +1,23 @@
+package ss3.exercise;
+
+public class Program {
+    public static void main(String[] args) {
+        int h = 6;
+        for (int i = 1; i < 2 * h; i++) {
+            for (int j = 1; j < 2 * h; j++) {
+                if (j == h - i || j == h + i) {
+                    System.out.print(i);
+                } else System.out.print(" ");
+            }
+            System.out.println();
+        }
+        for (int i = 2 * h; i > 1; i--) {
+            for (int j = 1; j < (2 * h) +1; j++) {
+                if (j == h - i || j == h + i) {
+                    System.out.print(i);
+                } else System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
